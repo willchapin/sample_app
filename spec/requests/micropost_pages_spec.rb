5593ba28842e8134fd627dbee2d488f3121dd553
willchapin <wrchapin@gmail.com>
2013-07-07 12:47:00 -0400

    change rspec tests

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -4,11 +4,6 @@ describe "Micropst Pages" do
   subject { page }
 
   let(:user) { FactoryGirl.create(:user) }
-  before(:all) { 50.times { FactoryGirl.create(:micropost, user: user) } }
-  after(:all) do 
-    Micropost.delete_all
-    User.delete_all
-  end
 
   before do
     sign_in(user)
@@ -34,14 +29,5 @@ describe "Micropst Pages" do
         expect { click_button 'Post' }.to change(Micropost, :count).by(1)
       end
     end
-  end
-
-  describe "micropost feed" do
-    it { should have_content('Micropost Feed') } 
-    it "should contain of the user's microposts" do
-      user.microposts.each do |micropost|
-        page.should have_selector('li', text: micropost.content)
-      end
-    end
-  end
+  end 
 end
