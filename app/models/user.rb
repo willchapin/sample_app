866ff5b3582a70a4c0d88ed69cb43a30fd8ed54f
willchapin <wrchapin@gmail.com>
2013-07-09 23:01:59 -0400

    add relationship helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: :follower_id, dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
   has_secure_password
-  
+
   before_save { email.downcase! }
   before_save :create_remember_token
 
-  
+
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false },
@@ -19,11 +20,23 @@ class User < ActiveRecord::Base
   def feed
     Micropost.where(user_id: id)
   end
-  
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
   private
-  
+
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64  
     end
-  
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,9 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:feed) }
   it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:following?) }
 
 
   it { should be_valid }
@@ -165,7 +168,26 @@ describe User do
       its(:feed) { should_not include(unfollowed_post) }
 
     end
- 
-    
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "and unfollowing" do
+      before do
+        @user.unfollow!(other_user)
+      end
+
+    it { should_not be_following(other_user) }
+    its(:followed_users) { should_not include(other_user) }
+
+    end
   end
 end
